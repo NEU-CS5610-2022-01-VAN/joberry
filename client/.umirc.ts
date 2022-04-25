68a3a28156daf399e609e604b8c606c8864f91d2
littlerzrz <renxinyi.design@gmail.com>
2022-04-25 15:17:55 -0700

    switched back to create-react-app

--- client/.umirc.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'umi';
-
-export default defineConfig({
-  nodeModulesTransform: {
-    type: 'none',
-  },
-  locale: { antd: true },
-  fastRefresh: {},
-});
--- client/src/pages/profile/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './index.less';
-
-export default function Profile() {
-  return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
-  );
-}
