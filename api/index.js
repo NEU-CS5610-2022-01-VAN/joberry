96f19495f0324fed374576b15e0be97d1554db26
littlerzrz <renxinyi.design@gmail.com>
2022-04-25 23:26:50 -0700

    updates on schema and api;

--- api/index.js
+++ api/index.js
@@ -31,6 +31,7 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
+// ============================= USER ===================================
 // verify user status, if not registered in our database we will create it
 app.post("/verify-user", requireAuth, async (req, res) => {
   const auth0Id = req.user.sub;
@@ -57,7 +58,26 @@ app.post("/verify-user", requireAuth, async (req, res) => {
   }
 });
 
-app.get("/posts", requireAuth, async (req, res) => {
+// get Profile information of authenticated user
+app.get("/profile", requireAuth, async (req, res) => {
+  const auth0Id = req.user.sub;
+  const user = await prisma.user.findUnique({
+    where: {
+      auth0Id,
+    },
+    include: {
+      posts: true,
+      berries: true,
+      comments: true,
+    },
+  });
+
+  res.json(user);
+});
+
+// ============================= POSTS ==================================
+// get all posts
+app.get("/posts", async (req, res) => {
   const posts = await prisma.post.findMany();
   res.json(posts);
 });
@@ -66,12 +86,141 @@ app.get("/posts", requireAuth, async (req, res) => {
 app.post("/posts", requireAuth, async (req, res) => {
   const auth0Id = req.user.sub;
   const { title, body } = req.body;
-  const newItem = await prisma.post.create({
+  const newPost = await prisma.post.create({
     data: {
       title,
       body,
       author: { connect: { auth0Id } },
     },
   });
-  res.status(201).json(newItem);
+  res.status(201).json(newPost);
 });
+
+// get a post by id
+app.get("/posts/:id", requireAuth, async (req, res) => {
+  const id = req.params.id;
+  const post = await prisma.post.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      comments: true,
+      berries: true,
+    },
+  });
+  res.json(post);
+});
+
+// updates a post by id
+app.put("/posts/:id", requireAuth, async (req, res) => {
+  const id = req.params.id;
+  const { title, body } = req.body;
+  const updatedPost = await prisma.post.update({
+    where: {
+      id,
+    },
+    data: {
+      title,
+      body,
+    },
+  });
+  res.json(updatedPost);
+});
+
+// deletes a post item by id
+app.delete("/posts/:id", requireAuth, async (req, res) => {
+  const id = req.params.id;
+  const deletedPost = await prisma.post.delete({
+    where: {
+      id,
+    },
+  });
+  res.json(deletedPost);
+});
+
+// ============================= BERRIES ==============================
+// creates a berry
+app.post("/berries", requireAuth, async (req, res) => {
+  const auth0Id = req.user.sub;
+  const { postId } = req.body;
+  const newBerry = await prisma.berry.create({
+    data: {
+      postId,
+      userId: { connect: { auth0Id } },
+    },
+  });
+  res.status(201).json(newBerry);
+});
+
+// gets all berries of a post
+app.get("berries", requireAuth, async (req, res) => {
+  const { postId } = req.body;
+  const berries = await prisma.berry.findMany({
+    where: {
+      postId,
+    },
+    include: {
+      user: true,
+    },
+  });
+  res.status(201).json(berries);
+});
+
+// delete a berry
+app.delete("/berries", requireAuth, async (req, res) => {
+  const auth0Id = req.user.sub;
+  const { postId } = req.body;
+  const deletedBerry = await prisma.berry.delete({
+    where: {
+      postId,
+      userId: { connect: { auth0Id } },
+    },
+  });
+  res.status(201).json(deletedBerry);
+});
+
+// ============================= COMMENTS ==============================
+// creates a comment
+app.post("/comments", requireAuth, async (req, res) => {
+  const auth0Id = req.user.sub;
+  const { content, parentId } = req.body;
+  const newComment = await prisma.comment.create({
+    data: {
+      content,
+      parentId,
+      author: { connect: { auth0Id } },
+    },
+  });
+  res.status(201).json(newComment);
+});
+
+// gets all comments of a post
+app.get("/comments", requireAuth, async (req, res) => {
+  const { postId } = req.body;
+  const comments = await prisma.comment.findMany({
+    where: {
+      postId,
+    },
+    include: {
+      user: true,
+      childrenComments: {
+        include: {
+          childrenComments: true
+        }
+      }
+    },
+  });
+  res.status(201).json(comments);
+});
+
+//delete a comment
+app.delete("/comments/:id", requireAuth, async (req, res) => {
+  const id = req.params.id;
+  const deletedComments = await prisma.comment.deleteMany({
+    where: {
+      id,
+      parentId
+    }
+  })
+  res.status(201).json(deletedComments);
+})
