779660c86bdfdaab6ce6e0f27a9d1f93d2695422
littlerzrz <renxinyi.design@gmail.com>
2022-04-26 22:19:47 -0700

    added configs for stores and other stuff

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# Joberry frontend
--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import App from './App';
-import "@/styles/index.less";
 import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
-import {AuthTokenProvider} from '@/utils';
+import { AuthTokenProvider } from "@/utils";
+import { Provider as MobxProvider } from "mobx-react";
+import * as stores from "@/stores";
+
+import "@/styles/index.less";
 
 function RequireAuth({ children }) {
   const { isAuthenticated, isLoading } = useAuth0();
-
   if (!isLoading && !isAuthenticated) {
     return <Navigate to="/" replace />;
   }
-
   return children;
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MobxProvider store={stores} {...stores}>
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN}
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        redirectUri={`${window.location.origin}/verify-user`}
+        audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+        scope={requestedScopes.join(" ")}
+      >
+        <AuthTokenProvider>
+          <BrowserRouter>
+            <Routes></Routes>
+          </BrowserRouter>
+        </AuthTokenProvider>
+      </Auth0Provider>
+    </MobxProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
