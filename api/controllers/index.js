4e2dff7462590e321a189182eeb8b39dd1a41365
littlerzrz <renxinyi.design@gmail.com>
2022-04-28 20:54:04 -0700

    updates on backend routes

---
+++ api/controllers/index.js
@@ -0,0 +1,5 @@
+import userController from './userController.js'
+import activityController from './activityController.js'
+import postController from './postController.js'
+
+export { userController, activityController, postController };
