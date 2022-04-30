628f63cae7a4a3dffb97a5df08f796b3f6df9adc
Xiaoben Yin <70068293+XiaobenYin@users.noreply.github.com>
2022-04-29 20:24:14 -0700

    added post details page

--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -5,29 +5,36 @@ import { AddComment } from '@/components';
 
 export default function PostDetails() {
     return (
-        <div>
+        <div className='white-container'>
             <div className='post-title'>POST TITLE</div>
             <Divider />
-            <div>
+            <div className='post-content'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, placeat quasi quod impedit ipsa blanditiis saepe distinctio iure rerum perspiciatis, a porro, deleniti at dolore? Sapiente, modi. Nemo, vitae eveniet.
             </div>
-            <div>
-
+            <div className='post-stat'>
+                <div className='post-berries'>
+                    BERRIES · {121}
+                </div>
+                <div>
+                    COMMENTS · {5}
+                </div>
             </div>
             <Divider />
-            <div>
-                
-            </div>
-            <div>
-                <Comment>
+            <div className='comments'>
+                <div className='comment-stat'>
+                    Comments · {5}
+                </div>
+                <div className='comments-set'>
                     <Comment>
-                        <Comment></Comment>
-                        <Comment></Comment>
+                        <Comment>
+                            <Comment></Comment>
+                            <Comment></Comment>
+                        </Comment>
                     </Comment>
-                </Comment>
-            </div>
-            <div>
-                <AddComment></AddComment>
+                </div>
+                <div className='comment-adder'>
+                    <AddComment></AddComment>
+                </div>
             </div>
         </div>
     )
