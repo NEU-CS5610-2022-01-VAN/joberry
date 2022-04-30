00845a5114e775393a687a7ae2444b5381564499
Chenru Wu <chenruwu@Chenrus-MacBook-Pro.local>
2022-04-29 22:31:44 -0700

    Add notfound test

---
+++ client/src/tests/NotFound.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import NotFound from "../components/NotFound";
+
+test("renders Not Found copy", () => {
+  render(<NotFound />);
+  expect(screen.getByText("Not Found page")).toBeInTheDocument();
+});
